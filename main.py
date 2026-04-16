51c4e494289b36d6919811399aee8e059e3310fb
Pradnyadange <pradnyadange07@gmail.com>
2026-04-16 17:35:57 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import cv2
+import mediapipe as mp # mediapipe is defined
+import pyautogui # python library to control your computer
+import os
+
+
+mp_hands = mp.solutions.hands
+mp_draw = mp.solutions.drawing_utils
+
+#hand dector
+hands = mp_hands.Hands(
+    model_complexity = 0,
+    max_num_hands=2,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5
+)
+
+#Open thw default webcam
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    success, frame = cap.read()
+    if not success :
+        break
+
+    #Mirror horizontally so the feed feels like looking in a mirror
+    frame = cv2.flip(frame,1)
+
+#Conversion of BGR TO RGB
+    rgb_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+
+#Run hand
+    results = hands.process(rgb_frame)
+
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_draw.draw_landmarks(
+                frame,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS
+            )
+
+    cv2.imshow("Hand Landmarks" , frame)
+
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+    cap.release()
+    cv2.destroyAllWindows()
+
